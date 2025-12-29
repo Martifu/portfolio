e399eee1b83f3653af1ab96ac27c3c5d00b0c50b
martifu <martin0013esp@hotmail.com>
2025-12-29 16:03:19 -0600

    config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   images: {
+    unoptimized: true,
     domains: ["framerusercontent.com"],
   },
 };
 
 export default nextConfig;
+
